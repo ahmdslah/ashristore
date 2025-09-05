416cf0dd8e4ca805746b8995ee8270e74ac3bc66
ahmed salah <osrhma@gmail.com>
2025-09-05 07:56:13 +0300

    splash

---
+++ lib/splash.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'dart:async';
+
+class VideoSplashScreen extends StatefulWidget {
+  @override
+  _VideoSplashScreenState createState() => _VideoSplashScreenState();
+}
+
+class _VideoSplashScreenState extends State<VideoSplashScreen> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(
+        "assets/vedio/Untitled Project/Untitled Project.mp4",
+      )
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+
+    // بعد انتهاء الفيديو روح للهوم
+    Timer(Duration(seconds: 5), () {
+      Navigator.pushReplacementNamed(context, "/home");
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+          _controller.value.isInitialized
+              ? SizedBox.expand(
+                child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: SizedBox(
+                    width: _controller.value.size.width,
+                    height: _controller.value.size.height,
+                    child: VideoPlayer(_controller),
+                  ),
+                ),
+              )
+              : Center(child: CircularProgressIndicator()),
+    );
+  }
+}
